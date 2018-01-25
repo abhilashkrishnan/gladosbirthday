8bb8c15ad10ac249981cf4c378c7eb7bb53ebadf
Abhilash Krishnan <abhilashk.cse@gmail.com>
2018-01-26 00:27:40 +0530

    GLADO'S BIRTHDAY PROGRAMMING CHALLENGE

---
+++ src/main/java/com/gladosbday/EatCake.java
@@ -0,0 +1,21 @@
+package com.gladosbday;
+
+public class EatCake {
+	public static void main(String[] args) {
+		
+		int[] children = new int[26];
+		
+		for (int idx = 0; idx < children.length; idx++) {
+			children[idx] = idx + 1;
+		}
+		
+		int age = 9;
+		
+		PlayCircle pc = new PlayCircle();
+		pc.formCircle(children);
+		pc.removeChild(age);
+		int pos = pc.getPosition();
+		pc.printPosition(pos);
+		pc.printToOutputStream(pos);
+	}
+}
